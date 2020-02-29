6c1db26a52b7f071c5167c895ebd431dc2d2004f
jambompeople <2572432388@qq.com>
2020-02-28 19:52:44 -0500

    potato update

---
+++ MyClass.java
@@ -0,0 +1,20 @@
+public class MyClass{
+  public static void main(String[]args){
+    student JacksonDing = new student();
+    JacksonDing.FieldTrip(7,"JacksonDing");
+  }
+}
+class student{
+  int grade;
+  int id;
+  String name;
+  public void FieldTrip(int grade, String name){
+    this.grade = grade;
+    this.name = name;
+    if(grade==7){
+      System.out.println(name+"\tcan go to the FieldTrip");
+    }else{
+      System.out.println(name+"can not go to the FieldTrip");
+    }
+  }
+}
