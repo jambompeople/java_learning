5a1981441cddc53d765044a811463902400e2226
jambompeople <you@example.com>
2020-06-07 18:29:36 -0400

    added a class

--- classjacksonding.java
+++ classjacksonding.java
@@ -1,8 +1,12 @@
 import java.util.*;
 public class classjacksonding{
   public static void main(String[]args){
-    public class triangle(){
-      
+    public class triangle{
+      int side1 = 0;
+      int side2 = 0;
+      int side3 = 0;
+      int base = 0;
+      int hight = 0;
     }
   }
 }
