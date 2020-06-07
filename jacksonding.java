c4a10555f1e4332d6304a5d33d8f0579906e4c87
jambompeople <you@example.com>
2020-06-07 12:15:38 -0400

    finish the triangle function

--- jacksonding.java
+++ jacksonding.java
@@ -1,9 +1,6 @@
 import java.util.*;
 public class jacksonding{
   static void squares(){
-    Scanner MyAmount = new Scanner(System.in);
-    System.out.println("please put how many rectangle you have");
-    int amount = MyAmount.nextInt();
     Scanner MyLength = new Scanner(System.in);
     System.out.println("please put the length of one of the rectangle you have");
     int length = MyLength.nextInt();
@@ -27,8 +24,26 @@ public class jacksonding{
     Scanner base = new Scanner(System.in);
     System.out.println("please put your base of the triangle here");
     int mybase = base.nextInt();
+    Scanner hight = new Scanner(System.in);
+    System.out.println("please put the hight here");
+    int myhight = hight.nextInt();
+    Scanner sides = new Scanner(System.in);
+    System.out.println("please put the sides simultaneously");
+    int side1 = sides.nextInt();
+    int side2 = sides.nextInt();
+    int side3 = sides.nextInt();
+    int perimeter = side1+side2+side3;
+    int area = mybase*myhight/2;
+    Scanner Mychoice = new Scanner(System.in);
+    System.out.println("if you want to calculate area please type 1, if you want to calculate premeter please type 2 ");
+    int choice = Mychoice.nextInt();
+    if(choice == 1){
+      System.out.println("the area of the triangle is "+area);
+    }else if(choice == 2){
+      System.out.println("the perimeter of the triangle is "+perimeter);
+    }
   }
   public static void main(String[]args){
-    squares();
+    triangles();
    }
   }
