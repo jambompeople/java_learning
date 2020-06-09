cecdab869fa76235b29f5d478f39e3440dc32521
jambompeople <you@example.com>
2020-06-09 15:15:23 -0400

    I did a constructor to the class and functions

--- classjacksonding.java
+++ classjacksonding.java
@@ -1,12 +1,24 @@
 import java.util.*;
 public class classjacksonding{
   public static void main(String[]args){
-    public class triangle{
-      int side1 = 0;
-      int side2 = 0;
-      int side3 = 0;
-      int base = 0;
-      int hight = 0;
-    }
+    triangle jackson = new triangle(1,2,3);
+    jackson.perimeter();
+  }
+
+}
+class triangle{
+  private int side1;
+  private int side2;
+  private int side3;
+  private int base ;
+  private int hight;
+  public triangle(int side1,int side2,int side3){
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+  public void perimeter(){
+    int perimeter = side1+side2+side3;
+    System.out.println("the perimeter is " + perimeter);
   }
 }
