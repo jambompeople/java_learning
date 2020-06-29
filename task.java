11ce42233ba77c6c78800c1889f4dd0b14df6450
jambompeople <you@example.com>
2020-06-29 16:25:33 -0400

    figured out the input for the array but not sorting it withour using the sort function

---
+++ task.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class task{
+  public static void main(String[]args){
+      Scanner arrNum = new Scanner(System.in);
+      System.out.println("put the length of the array");
+      int arrnum = arrNum.nextInt();
+      int[] arr = new int[arrnum];
+
+      Scanner inNum1 = new Scanner(System.in);
+      for(int a = 0; a<arrnum; a++){
+        Scanner inNum = new Scanner(System.in);
+        System.out.println("please put a integer");
+        int myNum = inNum.nextInt();
+        arr[a]=myNum;
+      }
+      for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]);
+      }
+      for(int b=0;b<arr.length;b++){
+        for(int c=0;c<arr.length;c++){
+          if(arr[b]<arr[c]){
+
+          }
+        }
+      }
+  }
+}
